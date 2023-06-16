c5554010166976256ee6d2e5f41fcb2ec2df70ae
Rashedul Islam <rashed.tareq@gmail.com>
2023-06-16 11:01:49 +0600

    Magic Villa First API End point

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public List<VillaDTO> GetVillas()
+        {
+            return VillaStore.VillaList;
+        }
+
+        [HttpGet("{id}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.VillaList.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
